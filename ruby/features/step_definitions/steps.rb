5349046110107eb66fcd04f3f2b29d5411ee8cc7
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-02 16:26:13 +0200

    implement tax details assertion

--- ruby/features/step_definitions/steps.rb
+++ ruby/features/step_definitions/steps.rb
@@ -16,13 +16,14 @@ When(/^the tax is calculated$/) do
 	tax_relief = @child_count <= 2 ? @child_count * 10000 : @child_count * 33000
 	income_tax = income_tax > tax_relief ? income_tax - tax_relief : 0
 	@net_salary = @gross_salary - ssc - income_tax
+	@tax_details = [["Social Security Contributions", "Income Tax"], [ssc.to_i.to_s, income_tax.to_i.to_s]]
 end
 
 Then(/^the monthly net salary of the employee should be (\d+)$/) do |expacted_net_salary|
 	@net_salary.should eq(expacted_net_salary.to_i)
 end
 
-Then(/^the following taxes are paid by the employee$/) do |table|
-  # table is a Cucumber::Ast::Table
+Then(/^the following taxes are paid by the employee$/) do |expected_table|
+  expected_table.diff!(@tax_details)
 end
 
